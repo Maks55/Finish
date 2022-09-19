73ca2ac479bc7866648e8f59bd74d584e29911b4
Maks <maks.sungurov@yandex.ru>
2022-09-19 22:51:16 +0300

    added Блок схема

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+/*
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. При решении не реком-зндуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+*/
+
+string[] stringArray={"hello", "2", "world", "-)", "qwe"};
+Console.WriteLine($"{String.Join(", ",stringArray)}"); 
+string[] resultArray=NewArray(stringArray);
+Console.WriteLine($"[{String.Join(", ",resultArray)}]");
+
+string[] NewArray(string[] arr)
+{
+    string[] result=new string[]{};
+    int j=0;
+    for (int i=0;i<arr.Length;i++)
+    {
+        if (arr[i].Length<=3)
+        {
+            Array.Resize(ref result,result.Length+1);
+            result[j]=arr[i];
+            j++;
+        }
+    }
+    return result;
+}
